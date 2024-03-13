2b68d8d50dfcf9de84a82d5b2f0e7d7ef465f218
Arnold-In-Tech <g16a7782@gmail.com>
2024-03-13 22:12:32 +0300

    completed assignment

--- lib/debug.py
+++ lib/debug.py
@@ -6,4 +6,49 @@ from department import Department
 
 import ipdb
 
+Department.drop_table()  # deletes table
+
+Department.create_table()  # creates table with no data
+# CURSOR.execute("PRAGMA table_info(departments)").fetchall()   # PRAGMA statement: shows info about the created table
+
+#======== below is wrapped in create function ======
+# payroll = Department("Payroll", "Building A, 5th Floor")
+# print(payroll)  # <Department None: Payroll, Building A, 5th Floor>
+
+# payroll.save()  # Persist to db, assign object id attribute
+# print(payroll)  # <Department 1: Payroll, Building A, 5th Floor>
+
+# hr = Department("Human Resources", "Building C, East Wing")
+# print(hr)  # <Department None: Human Resources, Building C, East Wing>
+
+# hr.save()  # Persist to db, assign object id attribute
+# print(hr)  # <Department 2: Human Resources, Building C, East Wing>
+# ======================================================
+
+# ====== create functions
+payroll = Department.create("Payroll", "Building A, 5th Floor")
+print(payroll)  # <Department 1: Payroll, Building A, 5th Floor>
+
+hr = Department.create("Human Resources", "Building C, East Wing")
+print(hr)  # <Department 2: Human Resources, Building C, East Wing>
+# ======
+
+# ====== update
+hr.name = 'HR'
+hr.location = "Building F, 10th Floor"
+hr.update()
+print(hr)  # <Department 2: HR, Building F, 10th Floor>
+
+# ======= delete
+print("Delete Payroll")
+payroll.delete()  # delete from db table, object still exists in memory
+print(payroll)  # <Department 1: Payroll, Building A, 5th Floor>
+
 ipdb.set_trace()
+
+# ==== Querying the database to extract a list of items
+departments = CURSOR.execute('SELECT * FROM departments')
+[row for row in departments]							# This will return each row with assoc. info. in a tuple
+# => [(1, 'Payroll', 'Building A, 5th Floor'), (2, 'Human Resources', 'Building C, East Wing')]
+
+# exit()    # exits out of pdb
